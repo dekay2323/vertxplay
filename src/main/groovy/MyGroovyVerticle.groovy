44b499159d6b6a9176cb75385635df89b2090135
dekay2323 <dekay.2323@gmail.com>
2018-02-18 23:35:28 -0700

    Use routes, passing from one route to another

--- src/main/groovy/MyGroovyVerticle.groovy
+++ src/main/groovy/MyGroovyVerticle.groovy
@@ -4,11 +4,27 @@ import io.vertx.ext.web.Router
 def server = vertx.createHttpServer()
 def router = Router.router(vertx)
 
-router.route().handler({routingContext ->
-    HttpServerResponse response = routingContext.response()
-    response.putHeader('content-type', 'text/plain')
-    
-    response.end('Hello world from vert.x-web')
+
+def routeHome = router.route("/").handler({routingContext ->
+    def response = routingContext.response()
+    response.putHeader("content-type", "text/plain")
+    response.end("Hello World from Vert.x-Web!")
+})
+
+def routeHome1 = router.route("/home/").handler({routingContext -> 
+    def response = routingContext.response()
+    response.setChunked(true)
+    response.write("routeHome1\n")
+    routingContext.vertx().setTimer(5000, {tid ->
+        routingContext.next()
+    })
+})
+
+def routeHome2 = router.route("/home/").handler({routingContext ->
+    def response = routingContext.response()
+    response.setChunked(true)
+    response.write("routeHome2\n")
+    routingContext.response().end()
 })
 
 server.requestHandler(router.&accept).listen(8080)
