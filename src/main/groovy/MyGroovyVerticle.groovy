abad9c655bdeb8d7e0436bef04a2ecb56ed6c148
dekay2323 <dekay.2323@gmail.com>
2018-02-18 23:43:16 -0700

    Added a restful parameter route

--- src/main/groovy/MyGroovyVerticle.groovy
+++ src/main/groovy/MyGroovyVerticle.groovy
@@ -5,26 +5,36 @@ def server = vertx.createHttpServer()
 def router = Router.router(vertx)
 
 
-def routeHome = router.route("/").handler({routingContext ->
-    def response = routingContext.response()
+def routeHome = router.route("/").handler({routingHandler ->
+    def response = routingHandler.response()
     response.putHeader("content-type", "text/plain")
     response.end("Hello World from Vert.x-Web!")
 })
 
-def routeHome1 = router.route("/home/").handler({routingContext -> 
-    def response = routingContext.response()
+def routeHome1 = router.route("/home/").handler({routingHandler -> 
+    def response = routingHandler.response()
     response.setChunked(true)
     response.write("routeHome1\n")
-    routingContext.vertx().setTimer(5000, {tid ->
-        routingContext.next()
+    routingHandler.vertx().setTimer(5000, {tid ->
+        routingHandler.next()
     })
 })
 
-def routeHome2 = router.route("/home/").handler({routingContext ->
-    def response = routingContext.response()
+// Note the star means it will route on /home/hello/
+def routeHome2 = router.route("/home/*").handler({routingHandler ->
+    def response = routingHandler.response()
     response.setChunked(true)
     response.write("routeHome2\n")
-    routingContext.response().end()
+    routingHandler.response().end()
+})
+
+// Rest params
+def routeParams = router.route('/catalog/:name/:id').handler({routingHandler ->
+    def response = routingHandler.response()
+    response.putHeader("content-type", "text/plain")
+    def name = routingHandler.request().getParam('name');
+    def id = routingHandler.request().getParam('id')
+    response.end("Hello World name:${name} and id:${id}")
 })
 
 server.requestHandler(router.&accept).listen(8080)
