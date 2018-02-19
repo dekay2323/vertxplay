245045501812dbafe224a55c359fa05410aa0e91
dekay2323 <dekay.2323@gmail.com>
2018-02-19 11:08:45 -0700

    Route with regex param getting

--- src/main/groovy/MyGroovyVerticle.groovy
+++ src/main/groovy/MyGroovyVerticle.groovy
@@ -4,37 +4,51 @@ import io.vertx.ext.web.Router
 def server = vertx.createHttpServer()
 def router = Router.router(vertx)
 
-
-def routeHome = router.route("/").handler({routingHandler ->
-    def response = routingHandler.response()
+/**
+ * My implementations of http://vertx.io/docs/vertx-web/groovy/
+ * 
+ */
+router.route("/").handler({routingContext ->
+    def response = routingContext.response()
     response.putHeader("content-type", "text/plain")
     response.end("Hello World from Vert.x-Web!")
 })
 
-def routeHome1 = router.route("/home/").handler({routingHandler -> 
-    def response = routingHandler.response()
+router.route("/home/").handler({routingContext -> 
+    def response = routingContext.response()
     response.setChunked(true)
     response.write("routeHome1\n")
-    routingHandler.vertx().setTimer(5000, {tid ->
-        routingHandler.next()
+    routingContext.vertx().setTimer(5000, {tid ->
+        routingContext.next()
     })
 })
 
 // Note the star means it will route on /home/hello/
-def routeHome2 = router.route("/home/*").handler({routingHandler ->
-    def response = routingHandler.response()
+router.route("/home/*").handler({routingContext ->
+    def response = routingContext.response()
     response.setChunked(true)
     response.write("routeHome2\n")
-    routingHandler.response().end()
+    routingContext.response().end()
 })
 
 // Rest params
-def routeParams = router.route('/catalog/:name/:id').handler({routingHandler ->
-    def response = routingHandler.response()
+router.route('/catalog/:name/:id').handler({routingContext ->
+    def name = routingContext.request().getParam('name');
+    def id = routingContext.request().getParam('id')
+
+    def response = routingContext.response()
     response.putHeader("content-type", "text/plain")
-    def name = routingHandler.request().getParam('name');
-    def id = routingHandler.request().getParam('id')
     response.end("Hello World name:${name} and id:${id}")
+})
+
+// Refex param routing
+router.route().pathRegex('\\/([^\\/]+)\\/([^\\/]+)').handler({routingContext ->
+    def product = routingContext.request().getParam('param0')
+    def id = routingContext.request().getParam('param1')
+
+    def response = routingContext.response()
+    response.putHeader("content-type", "text/plain")
+    response.end("product:${product} and id:${id}")
 })
 
 server.requestHandler(router.&accept).listen(8080)
