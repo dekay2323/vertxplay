c2a70697e2d6a28827110c10e0bb4873215c9c31
dekay2323 <dekay.2323@gmail.com>
2018-02-19 13:00:19 -0700

    Consume Json and output Json

--- src/main/groovy/RestVerticle.groovy
+++ src/main/groovy/RestVerticle.groovy
@@ -1,5 +1,9 @@
+import groovy.json.JsonOutput
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Vertx
+import io.vertx.core.http.HttpMethod
+import io.vertx.core.logging.Logger
+import io.vertx.core.logging.LoggerFactory
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.handler.BodyHandler
@@ -9,6 +13,7 @@ import io.vertx.ext.web.handler.BodyHandler
  * Make sure the groovy directory is a source path in Intellij (or IDE)
  */
 class RestVerticle extends AbstractVerticle {
+    Logger logger = LoggerFactory.getLogger(RestVerticle)
 
     // Can be run in ide
     // Can be debugged
@@ -19,15 +24,23 @@ class RestVerticle extends AbstractVerticle {
 
     @Override
     void start() {
+        logger.debug 'start'
         Router router = Router.router(vertx)
         router.route().handler(BodyHandler.create())
-        router.get('/home/').handler(this.&home)
+        router.get('/home/')
+                .method(HttpMethod.POST)
+                .consumes('application/json')
+                .produces('application/json')
+                .handler(this.&home)
         vertx.createHttpServer().requestHandler(router.&accept).listen(8080)
     }
 
     private void home(RoutingContext routingContext) {
+        logger.debug 'home'
+        def inJson = routingContext.getBodyAsJson()
+        logger.debug "inJson = ${inJson}"
+        def outJson = [text: 'hello']
         def response = routingContext.response()
-        response.putHeader("content-type", "text/plain")
-        response.end("You are home")
+        response.end(JsonOutput.toJson(outJson))
     }
 }
