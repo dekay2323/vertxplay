39762bfd86d0aa98a072770fb795e31b7b1ea9af
dekay2323 <dekay.2323@gmail.com>
2018-02-24 12:06:45 -0700

    Actually redirects to Okta but there is a problem in my app there

--- src/main/groovy/AuthVerticle.groovy
+++ src/main/groovy/AuthVerticle.groovy
@@ -9,14 +9,16 @@ import io.vertx.core.logging.Logger
 import io.vertx.core.logging.LoggerFactory
 import io.vertx.ext.auth.oauth2.OAuth2Auth
 import io.vertx.ext.auth.oauth2.OAuth2ClientOptions
+import io.vertx.ext.auth.oauth2.OAuth2FlowType
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.handler.AuthHandler
 import io.vertx.ext.web.handler.OAuth2AuthHandler
 
 /**
- * Make sure the groovy directory is a source path in Intellij (or IDE)
  * https://developer.okta.com/blog/2018/01/11/sso-oauth-vertx
+ * localhost:8080/
+ * localhost:8080/private/secret
  */
 class AuthVerticle extends AbstractVerticle {
     Logger logger = LoggerFactory.getLogger(AuthVerticle)
@@ -30,23 +32,9 @@ class AuthVerticle extends AbstractVerticle {
 
     @Override
     void start() throws Exception {
-        Router router = Router.router(vertx);
-
-        vertx.exceptionHandler(new Handler<Throwable>() {
-            @Override
-            void handle(Throwable event) {
-                System.err.write("Exception has been thrown : " + event.printStackTrace());
-            }
-        })
-
-        // Load in the config
-        ConfigStoreOptions fileStore = new ConfigStoreOptions()
-                .setType("file")
-                .setConfig(new JsonObject().put("path", "application.json"));
-        ConfigRetrieverOptions options = new ConfigRetrieverOptions()
-                .addStore(fileStore);
-        ConfigRetriever retriever = ConfigRetriever.create(vertx, options);
-        retriever.getConfig({ ar ->
+        logger.debug("start")
+        handleExceptions()
+        loadConfigurationFile().getConfig({ ar ->
             if (ar.failed()) {
                 logger.error("failed to retrieve config.");
             } else {
@@ -55,31 +43,47 @@ class AuthVerticle extends AbstractVerticle {
                 startServer();
             }
         })
-        logger.info("start")
     }
 
-    void startServer() {
-        logger.info("startServer")
+    private ConfigRetriever loadConfigurationFile() {
+        logger.debug("loadConfigurationFile")
+        ConfigStoreOptions fileStore = new ConfigStoreOptions()
+                .setType("file")
+                .setConfig(new JsonObject().put("path", "application.json"));
+        ConfigRetrieverOptions options = new ConfigRetrieverOptions()
+                .addStore(fileStore);
+        ConfigRetriever retriever = ConfigRetriever.create(vertx, options);
+        retriever
+    }
 
+    private Vertx handleExceptions() {
+        logger.debug("handleExceptions")
+        vertx.exceptionHandler(new Handler<Throwable>() {
+            @Override
+            void handle(Throwable event) {
+                System.err.write("Exception has been thrown : " + event.printStackTrace());
+            }
+        })
+    }
+
+    private void startServer() {
+        logger.debug("startServer")
         Router router = Router.router(vertx);
-
         router.route('/').handler(this.&home)
         AuthHandler authHandler = getOAuthHandler(router)
         router.route("/private/*").handler(authHandler)
-        router.route("/private/secret")
-                .handler({ ctx ->
-            ctx.response().end("Hi");
-        })
+        router.route("/private/secret").handler({ ctx -> ctx.response().end("Hi") })
 
         vertx.createHttpServer()
                 .requestHandler(router.&accept)
                 .listen(config().getInteger("port"))
     }
 
-    AuthHandler getOAuthHandler(Router router) {
+    private AuthHandler getOAuthHandler(Router router) {
+        logger.debug("getOAuthHandler")
         OAuth2Auth oauth2 = OAuth2Auth.create(vertx, OAuth2FlowType.AUTH_CODE, new OAuth2ClientOptions()
-                .setClientID(config().getString("0oae4muh0jw1QZ9Ej0h7"))
-                .setClientSecret(config().getString("-LkEK0YLeNhfKWo45uWYywz7nbDan3IQmixQ10jH"))
+                .setClientID(config().getString("clientId"))
+                .setClientSecret(config().getString("clientSecret"))
                 .setSite(config().getString("issuer"))
                 .setTokenPath("/v1/token")
                 .setAuthorizationPath("/v1/authorize")
@@ -93,7 +97,6 @@ class AuthVerticle extends AbstractVerticle {
         return authHandler
     }
 
-    // localhost:8080/
     private void home(RoutingContext routingContext) {
         logger.debug 'home'
         def response = routingContext.response()
