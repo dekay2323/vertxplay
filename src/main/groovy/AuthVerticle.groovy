600a0fa18a392313c020e940a37d3ad52998d22b
dekay2323 <dekay.2323@gmail.com>
2018-02-24 10:00:24 -0700

    Trying to do OAuth through Okta. Also fixed the build.gradle

---
+++ src/main/groovy/AuthVerticle.groovy
@@ -0,0 +1,81 @@
+import io.vertx.config.ConfigRetriever
+import io.vertx.config.ConfigRetrieverOptions
+import io.vertx.config.ConfigStoreOptions
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.Vertx
+import io.vertx.core.json.JsonObject
+import io.vertx.core.logging.Logger
+import io.vertx.core.logging.LoggerFactory
+import io.vertx.ext.auth.oauth2.OAuth2Auth
+import io.vertx.ext.auth.oauth2.OAuth2ClientOptions
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.handler.AuthHandler
+import io.vertx.ext.web.handler.OAuth2AuthHandler
+
+/**
+ * Make sure the groovy directory is a source path in Intellij (or IDE)
+ * https://developer.okta.com/blog/2018/01/11/sso-oauth-vertx
+ */
+class AuthVerticle extends AbstractVerticle {
+    Logger logger = LoggerFactory.getLogger(AuthVerticle)
+
+    // Can be run in ide
+    // Can be debugged
+    static void main(String[] args) {
+        Vertx vertx = Vertx.vertx()
+        vertx.deployVerticle(new AuthVerticle())
+    }
+
+    @Override
+    public void start() throws Exception {
+        // Load in the config
+        ConfigStoreOptions fileStore = new ConfigStoreOptions()
+                .setType("file")
+                .setConfig(new JsonObject().put("path", "application.json"));
+        ConfigRetrieverOptions options = new ConfigRetrieverOptions()
+                .addStore(fileStore);
+        ConfigRetriever retriever = ConfigRetriever.create(vertx, options);
+        retriever.getConfig({ ar ->
+            if (ar.failed()) {
+                logger.error("failed to retrieve config.");
+            } else {
+                config().mergeIn(ar.result());
+                startServer();
+            }
+        })
+        logger.info("start")
+    }
+
+    void startServer() {
+        logger.info("startServer")
+        Router router = Router.router(vertx);
+
+        AuthHandler authHandler = getOAuthHandler(router)
+        router.route("/private/*").handler(authHandler)
+        router.route("/private/secret")
+                .handler({ ctx ->
+            ctx.response().end("Hi");
+        });
+
+        vertx.createHttpServer()
+                .requestHandler(router.&accept)
+                .listen(config().getInteger("port"));
+    }
+
+    AuthHandler getOAuthHandler(Router router) {
+        OAuth2Auth oauth2 = OAuth2Auth.create(vertx, OAuth2FlowType.AUTH_CODE, new OAuth2ClientOptions()
+                .setClientID(config().getString("0oae4muh0jw1QZ9Ej0h7"))
+                .setClientSecret(config().getString("-LkEK0YLeNhfKWo45uWYywz7nbDan3IQmixQ10jH"))
+                .setSite(config().getString("issuer"))
+                .setTokenPath("/v1/token")
+                .setAuthorizationPath("/v1/authorize")
+                .setUserInfoPath("/v1/userinfo")
+                .setUseBasicAuthorizationHeader(false)
+        )
+
+        OAuth2AuthHandler authHandler = OAuth2AuthHandler.create(oauth2, config().getString("callbackUrl"))
+        authHandler.extraParams(new JsonObject("{\"scope\":\"openid profile email\"}"))
+        authHandler.setupCallback(router.route())
+        return authHandler
+    }
+}
